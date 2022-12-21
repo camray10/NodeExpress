039a18c5ec014b7c171b1f4aeaae7ce68ad6d2c1
Cameron Ray <rayc287@protonmail.com>
2022-12-21 04:51:58 -0500

    fixed broken-app

--- broken-app/app.js
+++ broken-app/app.js
@@ -2,15 +2,18 @@ const express = require('express');
 let axios = require('axios');
 var app = express();
 
-app.post('/', function(req, res, next) {
+app.post('/', async function(req, res, next) {
   try {
-    let results = req.body.developers.map(async d => {
+    // Wait for all requests to complete
+    let userData = await Promise.all(req.body.developers.map(async d => {
       return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+    }));
 
-    return res.send(JSON.stringify(out));
-  } catch {
+    // Map over the results array to create the output array
+    let output = userData.map(r => ({ name: r.data.name, bio: r.data.bio }));
+
+    return res.send(JSON.stringify(output));
+  } catch (err) {
     next(err);
   }
 });
--- broken-app/issues.md
+++ broken-app/issues.md
@@ -1,1 +1,5 @@
 # Broken App Issues
+- Promises in the results array are not yet resolved, and so their data field will not be available.
+    - refactored code using async/await
+- Gave more descriptive variable names
+- 
